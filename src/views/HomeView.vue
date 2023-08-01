898eb189f1fb7bfb569c4e3b3743d571a5f2775b
Hameed Owoade <hmd.dtl@gmail.com>
2023-08-01 02:02:08 +0100

    landing page building-step1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1>Home</h1>
+  <div class="top">
+      <div class="text">
+          <h3 class="welcome">Welcome to Chatter: A Haven for Text-Based Content</h3>
+          <p>Unleash the Power of Words, Connect with Like-minded Readers and Writers</p>
+          <button class="get-started">Get started</button>
+      </div>
+  </div>
+  
 </template>
 
 <script lang="ts">
@@ -16,5 +23,40 @@ export default defineComponent({
 </script>
 
 <style>
+.top {
+  display: flex;
+  width: auto;
+  height: 765px;
+  flex-shrink: 0;
+  background: url("../assets/images/chatter6.jpg"), lightgray 50% / cover no-repeat;
+  background-size: 1420px 765px;
+}
+
+.text {
+  color: white;
+  width: 100%;
+  height: 764px;
+  line-height: 30px;
+  padding: 216px 228px;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  background: rgba(0, 0, 0, 0.55);
+}
+
+h3 {
+  font-size: 35px;
+  /* gap: 26px; */
+}
+
+.get-started {
+  padding: 8px 16px;
+  justify-content: center;
+  align-items: center;
+  /* gap: 8px; */
+  border-radius: 8px;
+  color: white;
+  background: #543EE0;
+}
 
 </style>
